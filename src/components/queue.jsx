0110a6494d1c05449dc7d21ad9430c8cd3e450de
Danielle Mallare <mallare-dani.d@husky.neu.edu>
2020-07-26 02:30:26 -0700

    Uses showQueue API to fetch a Queue

--- src/components/queue.jsx
+++ src/components/queue.jsx
@@ -1,16 +1,69 @@
 import React, { Component } from 'react';
 import { Table } from 'react-bootstrap';
+import graphQLFetch from '../GraphQLFetch';
 
 import QueueAdd from './queueadd';
 
+/*
+function QueueItem({ item }) {
+  return (
+    <tr>
+      <td>{item.id}</td>
+      <td>{item.name}</td>
+    </tr>
+  );
+}
+*/
+
 class Queue extends Component {
-  state = {
-    title: "Ellie's Office Hours"
+
+  constructor() {
+    super();
+    this.state = {
+      id: 2,
+      title: '',
+      description: '',
+      queueItems: []
+    }
+
+    this.loadData.bind(this);
   }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  async loadData() {
+
+    const query = `query showQueue(
+      $id: Int!
+    ) {
+      showQueue(
+        id: $id
+      ) {
+        title description
+        items {
+          name
+        }
+      }
+    }`;
+
+    const data = await graphQLFetch(query, {'id': this.state.id});
+    if (data) {
+      this.setState({
+        title: data.showQueue.title,
+        description: data.showQueue.description,
+        queueItems: data.showQueue.queueItems
+      });
+      console.log(this.state.title);
+    }
+  }
+
   render() {
     return (
       <>
         <h2>{this.state.title}</h2>
+        <h3>{this.state.description}</h3>
         <Table bordered condensed>
           <thead>
               <tr>
@@ -19,12 +72,6 @@ class Queue extends Component {
               </tr>
           </thead>
           <tbody>
-            <td>
-              1
-            </td>
-              <td>
-                Person A
-              </td>
           </tbody>
         </Table>
         <QueueAdd />
