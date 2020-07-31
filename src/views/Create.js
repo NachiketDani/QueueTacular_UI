33231d40283175692f44469fb83b95ebdd3fdb25
timegao <gao.tim@husky.neu.edu>
2020-07-31 16:15:54 -0700

    Adds state and minutes to Create.js

--- src/views/Create.js
+++ src/views/Create.js
@@ -11,10 +11,29 @@ import { Button, Card, CardBody, CardFooter, CardHeader, Col, Row,
 // TODO: Date and time field
 
 class Create extends React.Component {
-  constructor(props) {
-    super(props);
+constructor(props) {
+  super(props);
+  this.state = {
+    title: null,
+    description: null,
+    participants: null,
+    wait: null
   }
+}
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  handleInputChange = (event) => {
+    event.preventDefault();
+    this.setState({
+      [event.target.tiel]: event.target.value
+    })
+  }
+
   render() {
+    // const { title, description, participants, wait } = this.state;
     return (
       <div className="content">
         <Row>
@@ -22,10 +41,12 @@ class Create extends React.Component {
             <Card>
               <CardHeader>Create a Queue</CardHeader>
               <CardBody>
-                <Form>
+                <Form onSubmit={this.handleSubmit}>
                   <FormGroup>
+                    <p>{this.state.title}</p>
                     <Label for="createTitle">Title</Label>
-                    <Input placeholder="Title" type="text" id="createTitle" required/>
+                    <Input placeholder="Title" type="text" title="empty title"
+                    id="createTitle" onChange={this.handleInputChange}/>
                   </FormGroup>
                   <FormGroup>
                     <Label for="createDescription">Description</Label>
@@ -41,16 +62,28 @@ class Create extends React.Component {
                   </FormGroup>
                   <FormGroup>
                     <Label for="createParticipants">Maximum Wait Time</Label>
-                    <InputGroup>
-                      <Input placeholder="Maximum Wait Time" min={0} type="number" step="15" />
-                      <InputGroupAddon addonType="append">minutes</InputGroupAddon>
-                    </InputGroup>
+                    <Row>
+                      <Col>
+                        <InputGroup>
+                          <Input placeholder="Hours" min={0} type="number" step="1" />
+                          <InputGroupAddon addonType="append">hours</InputGroupAddon>
+                        </InputGroup>
+                      </Col>
+                      <Col>
+                        <InputGroup>
+                          <Input placeholder="Minutes" min={0} type="number" step="15" />
+                          <InputGroupAddon addonType="append">minutes</InputGroupAddon>
+                        </InputGroup>
+                      </Col>
+                    </Row>
+                  </FormGroup>
+                  <FormGroup>
+                    <CardFooter>
+                      <Button>Submit</Button>
+                    </CardFooter>
                   </FormGroup>
                 </Form>
               </CardBody>
-              <CardFooter>
-                <Button>Submit</Button>
-              </CardFooter>
             </Card>
           </Col>
         </Row>
