4f5c384731225b63683469e16b099bb3a929638f
TekihcaN <nachiketdani4587@gmail.com>
2020-07-31 16:10:14 -0700

    Adds table of queues, buttons to view

--- src/views/Join.js
+++ src/views/Join.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import AsyncSelect from "react-select/lib/Async";
 import graphQLFetch from "../GraphQLFetch.js";
+import { Table, Button } from "reactstrap";
 // import withToast from "./withToast";
 
 class Join extends React.Component {
@@ -34,10 +35,49 @@ class Join extends React.Component {
   render() {
     return (
       <div className="content">
+        <h4>Search your Queue-tacular ID:</h4>
         <AsyncSelect>
           loadOptions = {this.loadOptions}
           filterOption={() => true}
         </AsyncSelect>
+        <Table hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Queue Name</th>
+              <th>Currently Queued</th>
+              <th>Estimated Wait time(min)</th>
+              <th>Am I in this Queue??</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark's Haircutting services</td>
+              <td>4</td>
+              <td>45</td>
+              <td>
+                <Button>Join</Button>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Otto dentistry</td>
+              <td>3</td>
+              <td>90</td>
+              <td>
+                <Button>Join</Button>
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Larry's Shared Washing machine</td>
+              <td>3</td>
+              <td>35</td>
+              <td>Already Queued!</td>
+            </tr>
+          </tbody>
+        </Table>
       </div>
     );
   }
