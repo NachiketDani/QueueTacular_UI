4416726d56ba2639c3bb6a1f7643f999f8c0cae1
TekihcaN <nachiketdani4587@gmail.com>
2020-07-31 15:07:32 -0700

    Imported search component from proMERN, begins modifying for use to search queues based on identifier

--- src/views/Join.js
+++ src/views/Join.js
@@ -1,7 +1,35 @@
 import React, { Component } from "react";
+import AsyncSelect from "react-select/async";
+import graphQLFetch from "./GraphQLFetch.js";
+import withToast from "./withToast";
 
-class Join extends Component {
-  state = {};
+class Join extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onChangeSelection = this.onChangeSelection.bind(this);
+    this.loadOptions = this.loadOptions.bind(this);
+  }
+
+  onChangeSelection({ value }) {
+    const { history } = this.props;
+    history.push("/edit/${value}");
+  }
+
+  async loadOptions(term) {
+    if (term.length < 3) return [];
+    const query = `query issueList($search: String) {
+        issueList(search: $search) {
+        issues {id title}
+        }
+    }`;
+    const { showError } = this.props;
+    const data = await graphQLFetch(query, { search: term }, showError);
+    return data.issueList.issues.map((issue) => ({
+      label: `#${issue.id}: ${issue.title}`,
+      value: issue.id,
+    }));
+  }
+
   render() {
     return <h1>Hi</h1>;
   }
