788212f883a8291beb9540b3627d05f6b55738b2
TekihcaN <nachiketdani4587@gmail.com>
2020-08-04 22:46:57 -0700

    Working on search query for join function using react-select

--- src/views/Join.js
+++ src/views/Join.js
@@ -16,7 +16,7 @@ class Join extends React.Component {
   constructor(props) {
     super(props);
     this.onChangeSelection = this.onChangeSelection.bind(this);
-    // this.loadOptions = this.loadOptions.bind(this);
+    this.loadOptions = this.loadOptions.bind(this);
     this.loadData = this.loadData.bind(this);
   }
 
@@ -66,21 +66,23 @@ class Join extends React.Component {
 
   onChangeSelection() {}
 
-  // async loadOptions() {
-  //     if (term.length < 3) return [];
-  //     const query = `query issueList($search: String) {
-  //         issueList(search: $search) {
-  //         issues {id title}
-  //         }
-  // }
+  // Load options for search: needs 2 callbacks: loadOptions and filterOptions
+  // Each option is an object with a label and value: label is what user sees and value is the unique identifier
+  async loadOptions() {
+    if (term.length < 3) return [];
+    const query = `query issueList($search: String) {
+          issueList(search: $search) {
+          issues {id title}
+          }
+  }`;
 
-  //   const { showError } = this.props;
-  //   const data = await graphQLFetch(query, { search: term }, showError);
-  //   return data.issueList.issues.map((issue) => ({
-  //     label: `#${issue.id}: ${issue.title}`,
-  //     value: issue.id,
-  //   }));
-  // }
+    const { showError } = this.props;
+    const data = await graphQLFetch(query, { search: term }, showError);
+    return data.issueList.issues.map((issue) => ({
+      label: `#${issue.id}: ${issue.title}`,
+      value: issue.id,
+    }));
+  }
 
   render() {
     return (
