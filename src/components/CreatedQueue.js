0a3183e60b9e080db5091cfb22282af083699482
Danielle Mallare <mallare-dani.d@husky.neu.edu>
2020-08-11 21:52:23 -0700

    Added check that user is logged in

--- src/components/CreatedQueue.js
+++ src/components/CreatedQueue.js
@@ -7,9 +7,14 @@ import {
   CardHeader,
   CardBody,
   CardFooter,
+  CardTitle,
+  Row,
+  Col,
+  Progress,
   Badge,
   Button,
   Table,
+  Tooltip,
 } from 'reactstrap';
 
 import CreatedQueueParticipantHover from './CreatedQueueParticipantHover';
@@ -20,12 +25,14 @@ class CreatedQueue extends React.Component {
     super(props);
     this.state = {
       referrer: null,
+      items: props.items,
     };
   }
 
-  onDelete = (newItems, i) => {
-    // const items = newItems.filter((p) => p.item.id !== i);
-    // this.setState({ items });
+  onDelete = (item) => {
+    console.log('this works?');
+    const items = this.state.items.filter((c) => c.item !== item);
+    this.setState({ items });
   };
 
   tryRedirect = () => {
@@ -45,117 +52,114 @@ class CreatedQueue extends React.Component {
         />
       );
 
-    return (
-      <Card>
-        <CardBody>
-          <Table style={{ marginBottom: 0 }} size='sm' borderless>
-            <tbody>
-              <tr style={{ marginTop: 0, marginBottom: 0 }}>
-                <td
-                  style={{ marginTop: 0, marginBottom: 0, textAlign: 'right' }}
-                >
-                  <Button
-                    onClick={this.tryRedirect}
-                    style={{ marginRight: 10 }}
-                  >
-                    <i
-                      style={{ marginRight: 10 }}
-                      className='nc-icon nc-settings-gear-65'
-                    />
-                    Edit
-                  </Button>
-                  <Badge
-                    style={{ textAlign: 'right', verticalAlign: 'top' }}
-                    color='danger'
-                    onClick={() => this.props.removeCreated()}
-                    style={{ cursor: 'pointer' }}
-                  >
-                    <i className='nc-icon nc-simple-remove' />
-                  </Badge>
-                </td>
-              </tr>
-              <tr>
-                <td style={{ marginTop: 0, marginBottom: 0 }}>
-                  <h5
-                    style={{
-                      verticalAlign: 'top',
-                      textAlign: 'left',
-                      marginTop: 0,
-                      marginBottom: 0,
-                    }}
-                  >
+    let display =
+      this.props.loggedIn === true ? (
+        <Card>
+          <CardBody>
+            <Table style={{ marginBottom: 0 }} size='sm' borderless>
+              <tbody>
+                <tr>
+                  <CardHeader tag='h5' style={{ verticalAlign: 'top' }}>
                     {this.props.title}
-                  </h5>
-                </td>
-              </tr>
-            </tbody>
-          </Table>
-          <hr style={{ marginTop: 0 }} />
-          <Table style={{ marginBottom: 0 }} size='sm' borderless responsive>
-            <tbody>
-              <tr>
-                <td>{this.props.description}</td>
-              </tr>
-              <tr>
-                <td>
-                  <b>
-                    {this.props.status === 'Open' ? this.props.items.length : 0}
-                  </b>{' '}
-                  participant(s) currently waiting.
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  Queue is capped at <b>{this.props.maxParticipants}</b>{' '}
-                  participant(s).
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  <CreatedQueueParticipantHover items={this.props.items} />
-                </td>
-              </tr>
-              <tr>
-                <td>
-                  Estimated end of queue current wait time is <b>15</b> minutes.
-                </td>
-              </tr>
-              <tr>
+                  </CardHeader>
+                  <td style={{ textAlign: 'right' }}>
+                    <Button
+                      onClick={this.tryRedirect}
+                      style={{ marginRight: 10 }}
+                    >
+                      <i
+                        style={{ marginRight: 10 }}
+                        className='nc-icon nc-settings-gear-65'
+                      />
+                      Edit
+                    </Button>
+                    <Badge
+                      style={{ textAlign: 'right', verticalAlign: 'top' }}
+                      color='danger'
+                      onClick={() => this.props.removeCreated()}
+                      style={{ cursor: 'pointer' }}
+                    >
+                      <i className='nc-icon nc-simple-remove' />
+                    </Badge>
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+            <hr />
+            <Table style={{ marginBottom: 0 }} size='sm' borderless responsive>
+              <tbody>
+                <tr>
+                  <td>{this.props.description}</td>
+                </tr>
+                <tr>
+                  <td>
+                    <b>
+                      {this.props.status === 'Open'
+                        ? this.state.items.length
+                        : 0}
+                    </b>{' '}
+                    participant(s) currently waiting.
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    Queue is capped at <b>{this.props.maxParticipants}</b>{' '}
+                    participant(s).
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <CreatedQueueParticipantHover />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    Estimated end of queue current wait time is <b>15</b>{' '}
+                    minutes.
+                  </td>
+                </tr>
                 <td>
                   {this.props.status === 'Open' ? (
                     <Badge color='success'>
-                      <div style={{ marginLeft: 10, marginBottom: 0 }}>
+                      <h5 style={{ marginLeft: 10, marginBottom: 0 }}>
                         Active.
                         <i
                           style={{ marginRight: 10 }}
                           className='nc-icon nc-bulb-63'
                         />
-                      </div>
+                      </h5>
                     </Badge>
                   ) : (
                     <Badge color='danger'>
-                      <div style={{ marginLeft: 10, marginBottom: 0 }}>
+                      <h5 style={{ marginLeft: 10, marginBottom: 0 }}>
                         Closed
                         <i
                           style={{ marginRight: 10 }}
                           className='nc-icon nc-time-alarm'
                         />
-                      </div>
+                      </h5>
                     </Badge>
                   )}
                 </td>
-              </tr>
-            </tbody>
-          </Table>
-          <hr style={{ marginBottom: 0, marginTop: 0 }} />
-        </CardBody>
-        <CardFooter>
-          <div>
-            <ExpandableTable {...this.props} onDelete={this.onDelete} />
-          </div>
-        </CardFooter>
-      </Card>
-    );
+              </tbody>
+            </Table>
+            <hr style={{ marginBottom: 0, marginTop: 0 }} />
+          </CardBody>
+          <CardFooter>
+            <div>
+              <ExpandableTable
+                {...this.props}
+                items={this.state.items}
+                onDelete={this.onDelete}
+              />
+            </div>
+            <div style={{ textAlign: 'right' }}>
+              <i className='fa fa-history' /> Updated 3 mins ago
+            </div>
+          </CardFooter>
+        </Card>
+      ) : null;
+    return display;
   }
 }
 
--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -94,6 +94,7 @@ class Dashboard extends React.Component {
             <CreatedQueue
               removeCreated={this.removeCreated}
               {...this.props.createdQueues[this.state.showId]}
+              loggedIn={this.state.loggedIn}
             />
           ) : null}
         </div>
